eb1deeca3b4c3a1d60c9bdf33b6161feef38f68c
KevinPelletier <kev.pelletier@outlook.com>
2015-02-06 14:45:56 -0500

    francisation de lab1

--- lab1/opencv2/opencv2.cpp
+++ lab1/opencv2/opencv2.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include "device_launch_parameters.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/types_c.h>
@@ -22,8 +23,8 @@ Mat matrice;
 Mat matrice_PositionSouris;
 
 //Couleur des pixel
-Scalar PipetteColor;
-Scalar CurrentPixelColor;
+Scalar CouleurPipette;
+Scalar CouleurPixelEnCours;
 
 //Position Souris
 int MouseX;
@@ -31,95 +32,90 @@ int MouseY;
 char PositionColorSouris[500];
 
 //affiche text
-char MouseText[100];
-char ColorText[100];
+char TexteSouris[100];
+char TexteCouleur[100];
 
 //Camera
-bool CameraMode = false;
+bool ModeCamera = true;
 
 //7,8,10) Capteur de la souris
-void CallBackFunc(int event, int x, int y, int flags, void *userdata)
+void CallBackFunc(int event, int x, int y, int flags,void *userdata)
 {
 	MouseX = x;
 	MouseY = y;
 
-	if (event == EVENT_LBUTTONDOWN)
+	if(event == EVENT_LBUTTONDOWN)
 	{
 		//7) MÉMORISER LA COULEUR DE LA SOURIS
-		PipetteColor = CurrentPixelColor;
+		CouleurPipette = CouleurPixelEnCours;
 		//cout << "Left button of the mouse is clicked - position(" << x << "," << y  << ")" << endl;
 	}
-	else if (event == EVENT_RBUTTONDOWN)
+	else if(event == EVENT_RBUTTONDOWN)
 	{
 		//8) DESSINER UNE CROIX AVEC LA PIPETTE
 		int CrossSize = 20;
-
+		
 		//Ligne horizontal
-		Point P1(x - CrossSize / 2, y);
-		Point P2(x + CrossSize / 2, y);
+		Point P1 (x - CrossSize/2, y);
+		Point P2 (x + CrossSize/2, y);
 
 		//Ligne Verticale
-		Point P3(x, y - CrossSize / 2);
-		Point P4(x, y + CrossSize / 2);
+		Point P3 (x, y - CrossSize/2);
+		Point P4 (x, y + CrossSize/2);
 
-		line(matrice, P1, P2, PipetteColor);
-		line(matrice, P3, P4, PipetteColor);
-
-		CameraMode = false;
-		imwrite("SnapShot.png", matrice);
-
+		line(matrice,P1,P2,CouleurPipette);
+		line(matrice,P3,P4,CouleurPipette);
+		
+		ModeCamera = false;
+		imwrite("ImageEnCours.png",matrice);
+		
 		//cout << "Right button of the mouse is clicked - position(" << x << "," << y  << ")" << endl;
 	}
-	else if (event == EVENT_MOUSEMOVE)
+	else if(event == EVENT_MOUSEMOVE)
 	{
 		//cout << " Mouse over this position(" << x << "," << y  << ")" << endl;
 
 		Scalar Color(
-			matrice.at<cv::Vec3b>(MouseY, MouseX)[0],
-			matrice.at<cv::Vec3b>(MouseY, MouseX)[1],
-			matrice.at<cv::Vec3b>(MouseY, MouseX)[2],
+			matrice.at<cv::Vec3b>(MouseY,MouseX)[0],
+			matrice.at<cv::Vec3b>(MouseY,MouseX)[1],
+			matrice.at<cv::Vec3b>(MouseY,MouseX)[2],
 			255
 			);
-		CurrentPixelColor = Color;
+		CouleurPixelEnCours = Color;
 	}
 }
 
-void ReleaseImage()
-{
-	IplImage* src = cvLoadImage("breadfish.png");
-	cvReleaseImage(&src);
-};
 //Prendre une image
-void TakePicture(Mat matrice, bool &CameraMode)
+void PrendrePhoto(Mat matrice, bool &ModeCamera)
 {
-	imwrite("SnapShot.png", matrice);
-	CameraMode = false;
+	imwrite("ImageEnCours.png",matrice);
+	ModeCamera = false;
 }
 
 //4) DESSINER UNE DIAGONALE ROUGE PARTANT DUNE EXTREMITÉ
 //   VERT L'AUTRE
-void DrawRedDiagonal(Mat matrice, int Width, int Height)
+void DessineDiagoRouge(Mat matrice, int Largeur, int Hauteur)
 {
 	//Prends les 2 Extremite
-	Point P1(0, 0);
-	Point P2(Width, Height);
+	Point P1(0,0);
+	Point P2(Largeur,Hauteur);
 
 	//Couleur Rouge
-	Scalar Color(0, 0, 255);
+	Scalar Color(0,0,255);
 
-	line(matrice, P1, P2, Color);
+	line(matrice,P1,P2,Color);	
 };
 
 int main(int argc, char** argv)
 {
 	//Image
-	int Height;
-	int Width;
+	int Hauteur;
+	int Largeur;
 
 	//camera
 	VideoCapture camera(0);
-	bool HaveCamera = camera.isOpened();
-	namedWindow("Lab1", 1);
+	bool CameraTrouver = camera.isOpened();
+	namedWindow("Lab1",1);
 
 	//key Keyboard
 	int key;
@@ -129,102 +125,102 @@ int main(int argc, char** argv)
 	//matrice = imread("breadfish.png",CV_LOAD_IMAGE_COLOR);
 	//imshow("LoadSimpleImage",matrice);
 
-	while (Loop)
+	while(Loop)
 	{
 		key = waitKey(10);
-
+		
 		//capte la souris
-		setMouseCallback("Lab1", CallBackFunc, NULL);
+		setMouseCallback("Lab1",CallBackFunc,NULL);
 
 		//Avoir la longueur et la largeur de la matrice(Image) en cours
-		Height = matrice.rows;
-		Width = matrice.cols;
-
+		Hauteur = matrice.rows;
+		Largeur = matrice.cols;
+		
 		/****** MODE Camera *******/
-		if (CameraMode)
+		if(ModeCamera)
 		{
-			if (HaveCamera)
+			if(CameraTrouver)
 			{
 				//2) AFFICHE EN CONTINUE UNE IMAGE AVEC LA CAMÉRA
 				camera >> matrice;
-				//10) ECRIRE LA POSITION DE LA SOURIS SUR L'IMAGE
+					//10) ECRIRE LA POSITION DE LA SOURIS SUR L'IMAGE
 
-				int Offset = 50;
-				sprintf_s(MouseText, "(%d,%d)", MouseX, MouseY);
-				sprintf_s(ColorText, "(%i,%i,%i)", (int)CurrentPixelColor.val[0], (int)CurrentPixelColor.val[1], (int)CurrentPixelColor.val[2]);
+				int Decalage = 50;
+				sprintf(TexteSouris, "(%d,%d)", MouseX,MouseY);
+				sprintf(TexteCouleur, "(%i,%i,%i)", (int)CouleurPixelEnCours.val[0],(int)CouleurPixelEnCours.val[1],(int)CouleurPixelEnCours.val[2]);
+				string MT = TexteSouris;
+				string CT = TexteCouleur;
+				putText(matrice,MT + "=" + TexteCouleur ,Point(Largeur - (Decalage * 8.5), Hauteur - Decalage),FONT_HERSHEY_COMPLEX,1,Scalar(255,255,255,0),1);
 
-				string MT = MouseText;
-				string CT = ColorText;
-				putText(matrice, MT + "=" + ColorText, Point(Width - (Offset * 8.5), Height - Offset), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 255, 0), 1);
-
-				if (key == ENTER)
+				if(key == ENTER)
 				{
 					//3) PRENDRE UNE PHOTO ET RETOURNER EN MODE PHOTO
-					TakePicture(matrice, CameraMode);
+					PrendrePhoto(matrice,ModeCamera);
 				}
-				if (key == R)
+				if(key == R)
 				{
-					TakePicture(matrice, CameraMode);
+					PrendrePhoto(matrice,ModeCamera);
 
 					//4) DESSINER UNE DIAGONALE ROUGE
-					DrawRedDiagonal(matrice, Width, Height);
-					imwrite("SnapShot.png", matrice);
+					DessineDiagoRouge(matrice,Largeur,Hauteur);
+					imwrite("ImageEnCours.png",matrice);
 				}
 
-				imshow("Lab1", matrice);
+			
+				imshow("Lab1",matrice);
 			}
 			else
 			{
 				/*Test du changement de mode (lorsque	pas de camera)*/
-				matrice = imread("breadfish.png", CV_LOAD_IMAGE_COLOR);
-				if (key == ENTER)
-					CameraMode = false;
+				matrice = imread("breadfish.png",CV_LOAD_IMAGE_COLOR);
+				if(key == ENTER)
+					ModeCamera = false;
 			}
 		}
 		/******			 *******/
-
+		
 		/****** MODE Image *******/
 		else
 		{
-			matrice = imread("SnapShot.png", CV_LOAD_IMAGE_COLOR);
+			matrice = imread("ImageEnCours.png",CV_LOAD_IMAGE_COLOR);
 
-			if (key == R)
+			if(key == R)
 			{
 				//4) DESSINER UNE DIAGONALE ROUGE
-				DrawRedDiagonal(matrice, Width, Height);
-				imwrite("SnapShot.png", matrice);
+				DessineDiagoRouge(matrice,Largeur,Hauteur);
+				imwrite("ImageEnCours.png",matrice);
 			}
 		}
 		/******			 *******/
 
-		imshow("Lab1", matrice);
 		/****** MODE Menu *******/
-		if (key == M)
+		if(key == M)
 		{
 			//6) MODE MENU CONSOLE
 			string texteEntrer;
 			cout << "Nom de l'image a charger? ";
-			getline(cin, texteEntrer);
+			getline(cin,texteEntrer);
 			matrice = imread(texteEntrer, CV_LOAD_IMAGE_COLOR);
-
-			////A voir
-			//ReleaseImage();
-			imwrite("SnapShot.png", matrice);
+			imwrite("ImageEnCours.png",matrice);
+			ModeCamera = false;
 		}
 		/******			 *******/
 
 		//5) EFFACER L'IMAGE
-		if (key == ESPACE)
+		if(key == ESPACE)
 		{
-			ReleaseImage();
+			matrice = NULL;
+			imwrite("ImageEnCours.png",matrice);
 		}
 
 		//9) PASSER DU MODE Image AU MODE Camera
-		if (key == TAB)
+		if(key == TAB)
 		{
-			if (CameraMode)CameraMode = false;
-			else CameraMode = true;
+			if(ModeCamera)ModeCamera = false;
+			else ModeCamera = true;
 		}
+
+		imshow("Lab1",matrice);
 	}
 
 	//Liberation des ressources la caméra
